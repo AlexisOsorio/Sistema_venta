9e88c1bd50182342899a9248c18381d348a425ae
Kevin Osorio <kevinosorio201@gmail.com>
2022-06-16 22:07:05 -0500

    db

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
     $host = "localhost";
     $user = "root";
     $clave = "";
-    $bd = "facturacion";
+    $bd = "sis_venta";
 
     $conexion = mysqli_connect($host,$user,$clave,$bd);
     if (mysqli_connect_errno()){
